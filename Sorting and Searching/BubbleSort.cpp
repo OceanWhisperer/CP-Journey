51451986b7ffae44a7545f345044f024752cad7b
OceanWhisperer <61554424+OceanWhisperer@users.noreply.github.com>
2024-10-01 15:48:52 +0530

    worked on bubble sort algoritm

--- Sorting and Searching/BubbleSort.cpp
+++ Sorting and Searching/BubbleSort.cpp
@@ -1,8 +1,37 @@
+// Bubble Sort o(N^2)
+
+/*
+Approach
+
+- for each element compare with neighbouring element
+and adjsut position
+
+- Do this for each as long as result is obtained
+*/
+
+
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-     
-     
+
+
+	vector<int>v { 5,4,3,2,1};
+
+	int n= v.size();
+	
+	for(int times = 1; times <= n-1; times++) {
+		for(int j = 0; j <= n - times - 1; j++) {
+			if(v[j] > v[j + 1]) {
+				swap(v[j], v[j + 1]);
+			}
+		}
+	}
+
+    for(auto x : v) {
+    	cout << x<< " ";
+    }
+
+
 	return 0;
 }
