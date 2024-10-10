038f5c9de2319a046a7cdf4a3c4596c44a49d53a
OceanWhisperer <61554424+OceanWhisperer@users.noreply.github.com>
2024-10-10 20:07:19 +0530

    ds and predicate

---
+++ Number Thory/Prime Numbers/simplest.cpp
@@ -0,0 +1,12 @@
+/*
+Predicate Function is any such function that returns
+True or False.
+Binary Search works on monotonic Predicate Functions
+
+eg: TTTTFFFF & FFFFTTTT
+
+Aim: Devise a Monotonic Predicate Function so that 
+     it follows the above format.
+
+     Look for the first false / true element.
+*/
